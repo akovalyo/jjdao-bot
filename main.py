7b1ce0019edd220353e6b2b266f1c5c8e2188dcc
Alexander Kovalyov <58354349+akovalyo@users.noreply.github.com>
2023-02-24 03:36:48 -0600

    Upd

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
         for guild in guilds:
             print(f"{guild.name} - {guild.id}")
 
-    bot.load_extension("cogs.urlGuard")
+    #bot.load_extension("cogs.urlGuard")
     #bot.load_extension("cogs.updatePrice")
     bot.run(token)
 
