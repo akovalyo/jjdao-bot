d6d7399b83a308031d7ba677fc170ced8ce1f081
Alexander Kovalyov <58354349+akovalyo@users.noreply.github.com>
2023-02-24 01:17:42 -0600

    Create updatePrice.py
    Remove rascals coll

--- cogs/updatePrice.py
+++ cogs/updatePrice.py
@@ -37,7 +37,7 @@ class UpdatePrice(commands.Cog):
 
         jellyUsdChannel = self.bot.get_channel(self.jellyUsdChannelId)
         solUsdChannel = self.bot.get_channel(self.solUsdChannelId)
-        rascalsChannel = self.bot.get_channel(self.rascalsId)
+        # rascalsChannel = self.bot.get_channel(self.rascalsId)
         runawayChannel = self.bot.get_channel(self.runawayId)
 
         if jellyUsdChannel:
@@ -60,13 +60,13 @@ class UpdatePrice(commands.Cog):
             except Exception as e:
                 print(f"{e} - UpdatePrice-solChannel error")
 
-        if rascalsChannel:
-            self.me.updateCollectionStats()
-            try:
-                rascalsStr = f"Rascals: ◎{(self.me.rascalsFP / 10**9):.2f} ({self.me.rascalsListings})"
-                await rascalsChannel.edit(name=rascalsStr)
-            except Exception as e:
-                print(f"{e} - UpdatePrice-rascals channel")
+       # if rascalsChannel:
+       #     self.me.updateCollectionStats()
+       #     try:
+       #         rascalsStr = f"Rascals: ◎{(self.me.rascalsFP / 10**9):.2f} ({self.me.rascalsListings})"
+       #         await rascalsChannel.edit(name=rascalsStr)
+       #     except Exception as e:
+       #         print(f"{e} - UpdatePrice-rascals channel")
 
         if runawayChannel:
             try:
